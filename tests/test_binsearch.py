5e4c346bf2befc868fd63d4eab427b11f1b2d812
natthapong.ju <natthapong.ju@kmitl.ac.th>
2026-02-19 17:59:29 +0700

    Add test case for binary search

--- tests/test_binsearch.py
+++ tests/test_binsearch.py
@@ -5,3 +5,5 @@ from src.binsearch import binary_search
 def test_binary_search_basic():
     sample_list = [10,20,30,40,50]
     assert binary_search(sample_list, 10) == 0
+    assert binary_search(sample_list, 50) == 4
+    assert binary_search(sample_list, 60) == -1
